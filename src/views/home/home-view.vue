fb01566f2f6e7053300be45b41f7e9436654bb43
azykdev <azyk0106@gmail.com>
2024-03-21 16:47:12 +0500

    #3 - home navbar end

--- src/views/home/home-view.vue
+++ src/views/home/home-view.vue
@@ -1,15 +1,31 @@
 <template>
-  <div>
-    <h1>Home View</h1>
-  </div>
+  <header id="header" class="h-screen bg-slate-50">
+    <nav id="nav" class="bg-slate-100 h-[10vh] w-full">
+      <div id="nav-wrapper"
+        class="md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-x mx-auto py-3 flex items-center justify-between h-full">
+        <!-- logo -->
+        <router-link to="/">
+          <img src="@/assets/images/logo/selling-house-high-resolution-logo-transparent.png" alt="logo" class=" w-44">
+        </router-link>
+
+        <!-- nav links -->
+        <div class="">
+          <ul class="flex">
+            <li class="mx-3">
+              <router-link to="/login"
+                class=" text-lg font-semibold text-indigo-900 transition-colors border rounded-[5px] py-2 px-3 hover:bg-indigo-600 hover:text-indigo-50">Kirish</router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
 </template>
 
 <script>
-  export default {
-    name: 'HomeView'
-  }
+export default {
+  name: 'HomeView'
+}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    screens: {
+      sm: "600px",
+      md: "960px",
+      lg: "1280px",
+      xl: "1920px",
+      xxl: "2560px",
+    },
   },
   plugins: [],
 };
